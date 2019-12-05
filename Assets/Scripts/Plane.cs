77a3ab8831fd6986d71fdc477550e68debd473c0
tullrich <timothy.ullrich@gmail.com>
2019-12-05 17:27:08 -0600

    Add Plane prefab ready for spawning (it moves itself)

---
+++ Assets/Scripts/Plane.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour
+{
+    // Player movement speed
+    public Vector2 speed;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        // Stop moving if the game is over
+        if (GameManager.Instance.gameIsOver)
+            return;
+
+        // Calculate the amount to move this frame
+        Vector2 delteMove = speed * Time.fixedDeltaTime;
+
+        // Update rigidbody position
+        rb.MovePosition(rb.position + delteMove);
+    }
+}
