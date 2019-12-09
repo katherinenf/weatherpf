a58856d38ec652501e34f426ecf935ac16cb697e
tullrich <timothy.ullrich@gmail.com>
2019-12-09 16:09:04 -0600

    Add version and some movement to the main menu

---
+++ Assets/Scripts/VersionText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionText : MonoBehaviour
+{
+    void Start()
+    {
+        Text uiText = GetComponent<Text>();
+        if (uiText != null)
+        {
+            uiText.text = "v" + Application.version;
+        }
+    }
+}
